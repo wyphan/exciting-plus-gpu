263abacc0980c6b355debe15b0b1fa84b7275d7d
Wileam Phan <wphan@vols.utk.edu>
2019-05-23 18:09:53 -0400

    Added

---
+++ compile_gcc_cuda-hydra.sh
@@ -0,0 +1,22 @@
+# Compilers
+MAKE = make
+F90  = mpif90
+CUDA_PATH = /usr/local/cuda
+NVCC      = $(CUDA_PATH)/bin/nvcc
+
+# Make the binary
+cp make.inc.hydra.gcc.mkl.gpu make.inc
+make
+
+# Compile the necessary codes.
+$(NVCC) -c -g -G cublas_fortran.cu
+$(F90) -cpp -c -g cublas_fortran_iso.f90
+$(F90) -cpp -g -D_MPI_ -c -I./src/ genmegqblh_cublas.f90
+
+# Move the appropriate files over
+cp genmegqblh_cublas.o src/addons/expigqr/genmegqblh.o
+cp cublas_fortran_iso.o  cublas_fortran.o *.mod src/addons/expigqr/
+
+# re-Make the binary.
+cp make.inc.hydra.gcc.mkl.gpu make.inc
+make
