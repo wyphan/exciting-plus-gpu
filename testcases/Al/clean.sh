da3dbb0b47752eb2bba4d96d831d439b8c240515
Wileam Phan <wphan@vols.utk.edu>
2019-05-30 17:20:34 -0400

    Fixed testcase clean scripts

--- testcases/Al/clean.sh
+++ testcases/Al/clean.sh
@@ -2,3 +2,5 @@
 rm my_hosts *.OUT
 rm bandlines.dat bands.dat
 rm idos.dat tdos.dat
+rm -r q
+
--- testcases/FeTe/clean.sh
+++ testcases/FeTe/clean.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 rm my_hosts *.OUT
+rm bandlines.dat bands.dat
+rm idos.dat tdos.dat
+rm -r q
+
