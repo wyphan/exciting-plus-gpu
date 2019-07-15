daf3d761bcd543cb3259e4ff00a024f6378a467c
Wileam Phan <wphan@vols.utk.edu>
2019-07-15 19:29:53 -0400

    Fixed compilation without HDF5 on hydra

--- compile-hydra.sh
+++ compile-hydra.sh
@@ -6,9 +6,13 @@ export F90=mpif90
 export CUDA_PATH=/usr/local/cuda
 export NVCC=$CUDA_PATH/bin/nvcc
 
-# Make the binary
-cp make.inc.hydra.gcc.mkl.gpu make.inc
+# Clean up
 make clean
+rm *.o *.mod
+rm bin/elk-cpu bin/elk-gpu
+
+# Make the binary
+cp make.inc.hydra.gcc.mkl make.inc
 make
 
 # CPU-only version
@@ -21,7 +25,7 @@ $F90 -cpp -g -D_MPI_ -c -I./src/ genmegqblh_cublas.f90
 
 # Move the appropriate files over
 cp genmegqblh_cublas.o src/addons/expigqr/genmegqblh.o
-cp cublas_fortran_iso.o  cublas_fortran.o *.mod src/addons/expigqr/
+cp cublas_fortran_iso.o cublas_fortran.o cublas_f.mod src/addons/expigqr/
 
 # re-Make the binary.
 cp make.inc.hydra.gcc.mkl.gpu make.inc
