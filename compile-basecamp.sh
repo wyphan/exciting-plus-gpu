f48f91ebafea8e488c227b7989df2e992bbe0bc0
Wileam Phan <wphan@vols.utk.edu>
2020-06-02 13:30:37 -0400

    Add make.inc and compile script for BaseCamp with GCC

--- compile-basecamp.sh
+++ compile-basecamp.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
 about() {
-  echo "Exciting-Plus compile script for Summit (ORNL)"
-  echo "Last edited: May 6, 2020 (WYP)"
+  echo "Exciting-Plus compile script for wyp-BaseCamp"
+  echo "Last edited: June 2, 2020 (WYP)"
 }
-
-# Check whether script is executed from Summit login node
-curnode=`hostname --fqdn | awk 'BEGIN { FS ="." } ; { print $2 }'`
-if [ "x$curnode" != "xsummit" ]; then
-  echo "ERROR: script not executed on Summit"
-  exit 42 # Don't panic
-fi
 
 usage() { echo "Usage: $0 [compiler] [task]"; }
 
 tasklist() {
   echo "Available tasks:"
   echo "  help,"
-  echo "  elk, tau,"
+  echo "  elk,"
   echo "  pp, pp_u, pp_u4, spacegroup, utils"
   return 0
 } 
 
-# TODO: accomodate multiple compiler versions and extract them automatically
-IBMVER="IBM XL 16.1.1-5"
-PGIVER="PGI 19.9" # ", 20.1"
+GCCVER="GCC 9.3.0"
+PGIVER="PGI 19.10"
+LLVMVER="AOCC 2.1.0 / LLVM 9.0" # based on LLVM 9
 compilers() {
-  echo "On Summit, Exciting-Plus has been tested with the following compilers:"
-  echo "  ibm   ${IBMVER} (default compiler)"
-  echo "  pgi   ${PGIVER}"
-#  echo "  gcc   GCC 6.4.0"
-#  echo "  llvm  Clang/Flang 8.0.0+git"
+  echo "On BaseCamp, Exciting-Plus has been tested with the following compilers:"
+  echo "  gcc   ${IBMVER} (default compiler)"
+  #echo "  pgi   ${PGIVER}"
+  #echo "  llvm  ${LLVMVER}"
   return 0
 }
 
@@ -40,17 +32,17 @@ helptext() {
   echo "  help       Show this help text"
   echo
   echo "  elk        Compile Exciting-Plus"
-  echo "  tau        Compile Exciting-Plus with TAU 2.29.1 + chosen compiler"
+#  echo "  tau        Compile Exciting-Plus with TAU 2.29.1 + chosen compiler"
   echo
   echo "  pp         Compile 'bndchr' and 'pdos' utilities"
-  echo "  pp_u       Compile 'pp_u4' utility"
+#  echo "  pp_u       Compile 'pp_u4' utility"
   echo "  spacegroup Compile 'spacegroup' utility"
 #  echo "  eos        Compile 'eos' utility"
 #  echo "  plot3d     Compile 'sicvlm' and 'plot_wan_dens' utilities"
   echo "  utils      Compile all of the above utilities"
   echo
   echo "If no compiler choice is given, then the default compiler will be used."
-  echo "By default, these are turned on: MPI, OpenMP, ESSL, HDF5"
+  echo "By default, these are turned on: MPI, OpenMP, OpenBLAS"
   echo "Modify the appropriate 'make.inc' files for finer-grained control"
   echo "For now, please don't supply two compilers or two tasks"
   echo "TODO: improve compile script"
@@ -58,19 +50,14 @@ helptext() {
 
 # Default choices (can be overriden through environment variables)
 if [ "x$MAKE"     == "x"  ]; then MAKE=make; fi
-if [ "x$COMPILER" == "x"  ]; then COMPILER=ibm; fi
-if [ "x$USEESSL"  != "x0" ]; then export USEESSL=1; fi
-if [ "x$USEHDF5"  != "x0" ]; then export USEHDF5=1; fi
+if [ "x$COMPILER" == "x"  ]; then COMPILER=gcc; fi
+if [ "x$USEOBLAS" != "x0" ]; then export USEOBLAS=1; fi
+#if [ "x$USEHDF5"  != "x0" ]; then export USEHDF5=1; fi
 
 # Default choices
 export BUILDELK=1
 export BUILDUTILS=0
 export USETAU=0
-
-# Debugging shortcuts
-export EXCDIR=`pwd`
-export WANN="${EXCDIR}/src/addons/wann/"
-export EXPI="${EXCDIR}/src/addons/expigqr/"
 
 # Function to print '=' 80 times, adapted from this link
 # https://stackoverflow.com/questions/5349718/how-can-i-repeat-a-character-in-bash
@@ -102,14 +89,14 @@ parsetask() {
       ;;
 
   # Build instrumented Exciting-Plus for profiling with TAU
-    tau )
-      export USETAU=1
-      export COMPILER="tau-${COMPILER}"
-      return 0
-      ;;
+    #tau )
+      #export USETAU=1
+      #export COMPILER="tau-${COMPILER}"
+      #return 0
+      #;;
 
   # Compiler choice
-    ibm | pgi | gcc | llvm )
+    gcc | pgi | llvm )
       export BUILDELK=1
       export COMPILER="$1"
       return 0
@@ -157,91 +144,45 @@ elif [ "x$1" != "x" ]; then
   parsetask "$1"; if [ "x$?" != "x0" ]; then tasklist; exit 1; fi
 fi
 
-# ESSL depends on libxlf90_r
-# This function extracts IBM XL compiler paths and saves them to xlvars.sh
-getxlvars() {
-  module load xl
-  cat > summit-xlvars.sh << __EOF__
-export OLCF_XL_ROOT=${OLCF_XL_ROOT}
-export OLCF_XLF_ROOT=${OLCF_XLF_ROOT}
-export OLCF_XLC_ROOT=${OLCF_XLC_ROOT}
-export OLCF_XLMASS_ROOT=${OLCF_XLMASS_ROOT}
-export OLCF_XLSMP_ROOT=${OLCF_XLSMP_ROOT}
-__EOF__
-  chmod +x summit-xlvars.sh
-}
-
-# Note: no longer needed after Mar 10 2020 stack upgrade
-# PGI's OpenMP implementation relies on GCC's libatomic
-# This function extracts the GCC compiler path and saves it to xlvars.sh
-#getgccvars() {
-#  module load gcc
-#  echo "export OLCF_GCC_ROOT=${OLCF_GCC_ROOT}" > summit-gccvars.sh
-#  chmod +x summit-gccvars.sh
-#}
-
 # TODO: decouple tau options from compiler
 case ${COMPILER} in
 
-  ibm)
-    module load xl
-    export COMPILERVER="${IBMVER}"
-    ;;
-
-  pgi)
-    getxlvars
-    #getgccvars
-    module load pgi
-    export COMPILERVER="${PGIVER}"
-    #source ./summit-gccvars.sh
-    ;;
-
   gcc)
-    echo "Compiler not tested yet (TODO: rewrite make.inc.summit.gcc.cpu)"
+    export COMPILERVER="${GCCVER}"
+    ;;
+    
+  pgi)
+    echo "Compiler not yet tested (TODO: write make.inc.basecamp.pgi.cpu)"
     exit 1
-    #getxlvars
-    #module load gcc
-    #export COMPILERVER="${GCCVER}"
+    #module load pgi
+    #export COMPILERVER="${PGIVER}"
     ;;
 
   llvm)
-    echo "Compiler not tested yet (TODO: write make.inc.summit.llvm.cpu)"
+    echo "Compiler not yet tested (TODO: write make.inc.basecamp.llvm.cpu)"
     exit 1
-    #getxlvars
     #module load llvm
     #export COMPILERVER="${LLVMVER}"
     ;;
 
-  tau-ibm)
-    # TODO: Resolve ticket #419691
-    module load xl/16.1.1-5
-    export COMPILERVER="${IBMVER}"
-    export TAUVER="2.29.1"
-    module load tau/${TAUVER}
-    export TAU_MAKEFILE="${TAU_DIR}/lib/Makefile.tau-xl_16.1.1-5-papi-mpi"
-    module load papi
-    ;;
-
-  tau-pgi)
-    # TODO: Resolve ticket #419691 and test PGI 20.1
-    getxlvars # for ESSL
-    #getgccvars
-    module load pgi/19.9
-    export COMPILERVER="${PGIVER}"
-    export TAUVER="2.29.1"
-    module load tau/${TAUVER}
-    export TAU_MAKEFILE="${TAU_DIR}/lib/Makefile.tau-pgi_19.9-papi-mpi-pgi"
-    module load papi
-    #source ./summit-gccvars.sh
-    ;;
-
   tau-gcc)
-    echo "Compiler not yet tested (TODO: write make.inc.summit.tau-gcc.cpu)"
+    echo "Compiler not yet tested (TODO: write make.inc.basecamp.tau-gcc.cpu)"
     exit 1
     ;;
 
+  tau-pgi)
+    echo "Compiler not yet tested (TODO: write make.inc.basecamp.tau-pgi.cpu)"
+    exit 1
+    #module load pgi
+    #export COMPILERVER="${PGIVER}"
+    #export TAUVER="2.29.1"
+    #module load tau
+    #export TAU_MAKEFILE="${TAU_DIR}/lib/Makefile.tau-pgi-papi-mpi-pgi"
+    #module load papi
+    ;;
+
   tau-llvm)
-    echo "Compiler not yet tested (TODO: write make.inc.summit.tau-llvm.cpu)"
+    echo "Compiler not yet tested (TODO: write make.inc.basecamp.tau-llvm.cpu)"
     exit 1
     ;;
 
@@ -265,13 +206,6 @@ if [ "x${BUILDELK}" == "x1" ]; then
 
   echo; hline; echo
 
-  # Load IBM ESSL
-  if [ "x${USEESSL}" == "x1" ]; then
-    module load essl
-    echo "Using IBM ESSL"
-    if [ "${COMPILER:(-3)}" != "ibm" ]; then source ./summit-xlvars.sh; fi
-  fi
-
   # Load HDF5
   if [ "x${USEHDF5}" == "x1" ]; then
     module load hdf5
@@ -280,7 +214,7 @@ if [ "x${BUILDELK}" == "x1" ]; then
 
   # Copy the appropriate make.inc
   # TODO: Write the unavailable make.inc files
-  cp make.inc.summit.${COMPILER}.cpu make.inc
+  cp make.inc.basecamp.${COMPILER}.cpu make.inc
 
   # Extract link line from make.inc
   if [ "x${USETAU}" == "x1" ]; then
@@ -359,7 +293,6 @@ unset COMPILERVER
 unset BUILDELK
 unset BUILDUTILS
 unset UTILS
-unset USEESSL
 unset USEHDF5
 unset USETAU
 unset TAUVER
