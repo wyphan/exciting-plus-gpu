0344892c2212fb906a2be16456141f87d4d4b843
Wileam Phan <wphan@vols.utk.edu>
2020-09-04 15:16:53 -0400

    Revert mod_gpu::zgemm_batched_omp() to use array slices

--- src/addons/mod_gpu.f90
+++ src/addons/mod_gpu.f90
@@ -455,9 +455,9 @@ CONTAINS
 
        ! Call ZGEMM (let BLAS check the arguments)
        CALL zgemm( transA, transB, m, n, k, &
-            alpha, A_array(LBOUND(A_array,1),LBOUND(A_array,2),ibatch), ld1, &
-                   B_array(LBOUND(B_array,1),LBOUND(B_array,2),ibatch), ld2, &
-            beta,  C_array(LBOUND(C_array,1),LBOUND(C_array,2),ibatch), ld3 )
+            alpha, A_array(:,:,ibatch), ld1, &
+                   B_array(:,:,ibatch), ld2, &
+            beta,  C_array(:,:,ibatch), ld3 )
 
        ! Save result
 !       !$OMP CRITICAL
