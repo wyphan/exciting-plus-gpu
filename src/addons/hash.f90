2d274db1df91d28185fb4f6d6092c1106d4c6179
Ed D'Azevedo <dazevedoef@ornl.gov>
2020-09-04 16:10:02 -0400

    Avoid integer overflow in hash()

--- src/addons/hash.f90
+++ src/addons/hash.f90
@@ -1,12 +1,14 @@
-! DJBHash function
+! DJB Hash function
 ! http://www.partow.net/programming/hashfunctions/index.html
 integer function hash(str,len)
 implicit none
 character, intent(in) :: str(*)
 integer, intent(in) :: len
+integer, parameter :: nmax = 16777213 ! nearest prime to 2**24
 integer i
 hash=5381
 do i=1,len
   hash=hash*32+hash+ichar(str(i))
+  hash = mod( hash, nmax )
 enddo
 end function
