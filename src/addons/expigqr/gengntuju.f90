da42e244dc2ebbb134429dc90114a68976e4a22e
Wileam Phan <wphan@vols.utk.edu>
2020-10-19 14:14:49 -0400

    Cherry-pick f811af11 from cuda-hydra (DUMPgntuju compile flag with HDF5)

--- src/addons/expigqr/gengntuju.f90
+++ src/addons/expigqr/gengntuju.f90
@@ -3,12 +3,18 @@ use modmain
 use mod_addons_q
 use mod_expigqr
 use mod_util
+
+#ifdef _HDF5_
+USE mod_hdf5
+#endif /* HDF5 */
+
 implicit none
 ! arguments
 integer, intent(in) :: iq
 integer, intent(in) :: lmaxexp
 
 integer ig,is,ir,n,ias,io1,io2,l1,m1,lm1,l2,m2,lm2,l3,m3,lm3,ic
+
 ! for parallel
 integer igloc,ngqloc
 integer lmmaxexp
@@ -22,12 +28,113 @@ integer i,j,nuju,nujuloc,i1
 real(8), allocatable :: uju(:,:,:,:,:,:)
 logical, allocatable :: ujuflg(:,:,:,:,:)
 
+#ifdef _HDF5_
+
+CHARACTER(LEN=100) :: fname, pathq, pathqc, pathqcg
+CHARACTER(LEN=2) :: c2
+CHARACTER(LEN=3) :: c3
+CHARACTER(LEN=4) :: c4
+LOGICAL :: exist
+REAL(KIND(1.D0)), PARAMETER :: toKiB = 2.D0**(-10)
+REAL(KIND(1.D0)), PARAMETER :: toMiB = 2.D0**(-20)
+INTEGER :: bytes
+INTEGER, DIMENSION(3) :: gntyyydim, gntyyychunk
+INTEGER, DIMENSION(6) :: ujudim, ujuchunk
+INTEGER, DIMENSION(2) :: gntujudim, gntujuchunk
+
+#else
+
+#if defined(_DUMPgntyyy_) || defined(_DUMPuju_) || defined(_DUMPgntuju_)
+
+CHARACTER(LEN=32) :: refile
+CHARACTER(LEN=128) :: cmd
+
+#ifdef _DUMPgntyyy_
+CHARACTER(LEN=20) :: fmt1
+#endif /* DUMPgntyyy */
+
+#ifdef _DUMPuju_
+CHARACTER(LEN=20) :: fmt2
+#endif /* DUMPuju */
+
+#ifdef _DUMPgntuju_
+CHARACTER(LEN=32) :: imfile
+CHARACTER(LEN=17) :: fmt3
+INTEGER :: irow, icol
+#endif /* DUMPgntuju */
+
+#endif /* DUMP{gntyyy,uju,gntuju} */
+
+#endif /* HDF5 */
+
 lmmaxexp=(lmaxexp+1)**2
 allocate(jl(nrmtmax,0:lmaxexp))
 allocate(zm(0:lmaxexp,lmmaxapw,lmmaxapw))
 igntuju=0
 ngntuju=0
 gntuju=zzero
+
+#ifdef _DUMPgntyyy_
+IF( mpi_grid_root() ) THEN
+
+#ifdef _HDF5_
+   fname = "gntuju.hdf5"
+   INQUIRE( FILE=TRIM(fname), EXIST=exist )
+
+   ! Create file and populate data structure
+   IF ( (iq == 1) .AND. (.NOT. exist) ) CALL writegntujuheader( fname )
+
+   ! Dump gntyyy with HDF5
+   IF ( iq == 1 ) THEN
+      gntyyydim = (/ lmmaxvr, lmmaxapw, lmmaxapw /)
+      gntyyychunk(:) = gntyyydim(:)
+      bytes = hdf5_calc_chunksz( 'd', 3, gntyyychunk )
+      WRITE(*,*) 'Dumping gntyyy (', INT(REAL(bytes)*toKiB), ' KiB)'
+      CALL hdf5_gzwrite_array_d( gntyyy(1,1,1), 3, gntyyydim, gntyyychunk, 9, &
+           fname, "/", "gntyyy" )
+   END IF
+
+#else
+
+   ! Dump gntyyy
+   IF( iq == 1 ) THEN
+      WRITE(*,*)
+      WRITE(*,'("gntyyy( lm2, lm1, lm3 )")')
+      WRITE(*,'("lmmaxvr = ",I2)') lmmaxvr
+      WRITE(*,'("lmmaxapw = ",I2)') lmmaxapw
+      WRITE(*,*)
+
+      OPEN(UNIT = 666, FILE = "gntyyy.csv")
+      fmt1 = '(I2,5(",",I2),F10.6)'
+      WRITE(666,'("# L1, m1, L2, m2, L3, m3, gntyyy")')
+      DO L1 = 0, lmaxapw
+         DO m1 = -L1, L1
+            lm1 = idxlm(l1,m1)
+            DO L2 = 0, lmaxapw
+               DO m2 = -L2, L2
+                  lm2 = idxlm(l2,m2)
+                  DO L3 = 0, lmaxexp
+                     DO m3 = -L3, L3
+                        lm3 = idxlm(l3, m3)
+                        WRITE(666,fmt) L1, m1, L2, m2, L3, m3, &
+                                       gntyyy(lm3, lm2, lm1)
+                     END DO ! m3
+                     WRITE(666,*)
+                  END DO ! L3
+               END DO ! m2
+            END DO ! L2
+         END DO ! m1
+      END DO ! L1
+      CLOSE(666)
+      
+      WRITE(cmd, '("zip -9 -m -q gntyyy.zip gntyyy.csv)')
+      CALL EXECUTE_COMMAND_LINE(TRIM(cmd))
+      
+   END IF ! iq = 1
+
+#endif /* HDF5 */
+END IF ! wproc
+#endif /* DUMPgntyyy */
 
 ! total number of MT groups of radial integrals
 nuju=ngqsh(iq)*natmcls
@@ -69,6 +176,79 @@ do i=1,nujuloc
 enddo 
 deallocate(ujuflg)
 call mpi_grid_reduce(uju(0,0,0,1,1,1),(lmaxexp+1)*(lmaxapw+1)*(lmaxapw+1)*nufrmax*nufrmax*nuju,dims=(/dim_k/),all=.true.)
+
+#ifdef _DUMPuju_
+IF( mpi_grid_root() ) THEN
+   
+#ifdef _HDF5_
+
+   ! For now, only rank 0 writes using serial hdf5
+   ! TODO: Use MPI-IO (parallel hdf5)
+
+   fname = "gntuju.hdf5"
+   INQUIRE( FILE=TRIM(fname), EXIST=exist )
+
+   ! Create file and populate data structure
+   IF ( (iq == 1) .AND. (.NOT. exist) ) CALL writegntujuheader( fname )
+
+   WRITE( c3, '(I3.3)' ) iq
+   pathq = "/qpoints/" // c3
+
+   ! Dump uju( l3, l1, l2, io1, io2, (ic-1)*ngqsh(iq)+gqshidx(ig,iq) )
+      ujudim = (/ lmaxexp+1, lmaxapw+1, lmaxapw+1, nufrmax, nufrmax, nuju /)
+      ujuchunk(:) = ujudim(:)
+      bytes = hdf5_calc_chunksz( 'd', 6, ujuchunk )
+      WRITE(*,*) 'Dumping uju (', INT(REAL(bytes)*toKiB), ' KiB)'
+      CALL hdf5_gzwrite_array_d( uju(0,0,0,1,1,1), 6, ujudim, ujuchunk, 9, &
+           fname, pathq, "uju" )
+
+#else
+
+   IF( iq == 1 ) THEN
+      WRITE(*,*)
+      WRITE(*,'("uju( l3, l1, l2, io1, io2, &
+                &(ic-1)*ngqsh(iq)+gqshidx(ig,iq) )")')
+      WRITE(*,'("lmaxapw = ",I2)') lmaxapw
+      WRITE(*,'("nufrmax = ",I4)') nufrmax
+      WRITE(*,'("nuju = ngqsh(iq)*natmcls = ",I4)') nuju
+      WRITE(*,*)
+   END IF ! iq = 1
+
+   ! Dump uju
+   fmt2 = '(I2,4(",",I2),F10.6)'
+   DO ig = 1, ngq(iq)
+      DO ic = 1, natmcls
+         ias = ic2ias(ic) ! atom index
+         is = ias2is(ias) ! species index 
+         WRITE(refile, '("uju.iq",I2.2,".ig",I4.4,".ic",I2.2,".csv")') &
+                        iq, ig, ic
+         OPEN(UNIT = 666, FILE = TRIM(refile))
+         WRITE(666,'("# L1, L2, io1, io2, L3, uju")')
+         DO L1 = 0, lmaxapw
+            DO L2 = 0, lmaxapw
+               DO io1 = 1, nufr(L1,is)
+                  DO io2 = 1, nufr(L2,is)
+                     DO L3 = 0, lmaxexp
+                        WRITE(666,fmt2) L1, L2, io1, io2, L3, &
+                    uju( L3, L1, L2, io1, io2, (ic-1)*ngqsh(iq)+gqshidx(ig,iq) )
+                     END DO ! L3
+                     WRITE(666,*)
+                  END DO ! io2
+               END DO ! io1
+            END DO ! L2
+         END DO ! L1
+         CLOSE(666)
+         !-- TODO: There should be a better way than this
+         WRITE(cmd, '("zip -9 -m -q uju.zip ", A)') TRIM(refile)
+         CALL EXECUTE_COMMAND_LINE(TRIM(cmd))
+         !--
+      END DO ! ic
+   END DO ! ig
+
+#endif /* HDF5 */
+
+END IF ! wproc
+#endif /* DUMPuju */
 
 ngqloc=mpi_grid_map(ngq(iq),dim_k)
 do igloc=1,ngqloc
@@ -159,23 +339,193 @@ i=ngq(iq)/ngvb
 do ig=1,i
   call mpi_grid_reduce(gntuju(1,1,1,(ig-1)*ngvb+1),ngvb*ngntujumax*ngntujumax*natmcls,&
     &dims=(/dim_k/),all=.true.)
-  call mpi_grid_reduce(igntuju(1,1,1,(ig-1)*ngvb+1),ngvb*2*ngntujumax*natmcls,&
-    &dims=(/dim_k/),all=.true.)
-  call mpi_grid_reduce(ngntuju(1,(ig-1)*ngvb+1),ngvb*natmcls,dims=(/dim_k/),&
-    &all=.true.)    
+  ! Right now these are unused
+!  call mpi_grid_reduce(igntuju(1,1,1,(ig-1)*ngvb+1),ngvb*2*ngntujumax*natmcls,&
+!    &dims=(/dim_k/),all=.true.)
+!  call mpi_grid_reduce(ngntuju(1,(ig-1)*ngvb+1),ngvb*natmcls,dims=(/dim_k/),&
+!    &all=.true.)    
   call mpi_grid_barrier(dims=(/dim_k/))
 enddo
 do ig=i*ngvb+1,ngq(iq)
   call mpi_grid_reduce(gntuju(1,1,1,ig),ngntujumax*ngntujumax*natmcls,dims=(/dim_k/),&
-    &all=.true.)
-  call mpi_grid_reduce(igntuju(1,1,1,ig),2*ngntujumax*natmcls,dims=(/dim_k/),&
-    &all=.true.)
-  call mpi_grid_reduce(ngntuju(1,ig),natmcls,dims=(/dim_k/),all=.true.)    
+       &all=.true.)
+  ! Right now these are unused
+!  call mpi_grid_reduce(igntuju(1,1,1,ig),2*ngntujumax*natmcls,dims=(/dim_k/),&
+!    &all=.true.)
+!  call mpi_grid_reduce(ngntuju(1,ig),natmcls,dims=(/dim_k/),all=.true.)    
   call mpi_grid_barrier(dims=(/dim_k/))
 enddo
+
+#ifdef _DUMPgntuju_
+! Dump gntuju
+IF( wproc ) THEN
+
+#ifdef _HDF5_
+
+   ! For now, only rank 0 writes using serial hdf5
+   ! TODO: Use MPI-IO (parallel hdf5)
+
+   fname = "gntuju.hdf5"
+   INQUIRE( FILE=TRIM(fname), EXIST=exist )
+
+   ! Create file and populate data structure
+   IF ( (iq == 1) .AND. (.NOT. exist) ) CALL writegntujuheader( fname )
+
+   WRITE( c3, '(I3.3)' ) iq
+   pathq = "/qpoints/" // c3
+
+   ! Dump gntuju( lm2+(io2-1)*lmmaxapw, lm1+(io1-1)*lmmaxapw, ic, ig )
+   DO ic = 1, natmcls
+      WRITE( c2, '(I2.2)' ) ic
+
+      DO ig = 1, ngq(iq)
+         WRITE( c4, '(I4.4)' ) ig
+         pathqcg = TRIM(pathq) // "/class/" // c2 // "/gvectors/" // c4
+
+         gntujudim = (/ ngntujumax, ngntujumax /)
+         gntujuchunk = gntujudim
+         bytes = hdf5_calc_chunksz( 'z', 2, gntujuchunk )
+         WRITE(*,*) 'Dumping gntuju(:,:,ic=', ic, ',ig=', ig, ') (', &
+                    INT(REAL(bytes)*tokiB), ' kiB)'
+         CALL hdf5_gzwrite_array_z( gntuju(1,1,ic,ig), 2, &
+                                    gntujudim, gntujuchunk, 9, &
+                                    fname, pathqcg, "gntuju" )
+
+      END DO ! ig
+   END DO ! ic
+
+#else
+   
+   IF( iq == 1 ) THEN
+      WRITE(*,*)
+      WRITE(*,'("gntuju( lm2+(io2-1)*lmmaxapw, lm1+(io1-1)*lmmaxapw, &
+                           &ic, ig )")')
+      WRITE(*,'("lmaxapw = ",I2,", lmmaxapw = (lmaxapw+1)**2 = ",I4)') &
+                 lmaxapw, lmmaxapw
+      WRITE(*,'("nufrmax = ",I4)') nufrmax
+      WRITE(*,'("ngntujumax = lmmaxapw*nufrmax = ",I4)') ngntujumax
+      WRITE(*,*)
+   END IF ! iq = 1
+   WRITE(*,'("ngq(iq=",I2,")=",I4)') iq, ngq(iq)
+   WRITE(fmt3, '("(",I4.4,"(F10.6,'',''))")') ngntujumax
+   DO ig = 1, ngq(iq)
+      DO ic = 1, natmcls
+         WRITE(refile, '("gntuju.iq",I2.2,".ic",I2.2,".ig",I4.4,"_re.csv")') &
+              iq, ic, ig
+         WRITE(imfile, '("gntuju.iq",I2.2,".ic",I2.2,".ig",I4.4,"_im.csv")') &
+              iq, ic, ig
+         OPEN(UNIT = 666, FILE = TRIM(refile))
+         DO irow = 1, ngntujumax
+            WRITE(666,fmt3)( DREAL(gntuju(irow,icol,ic,ig)),icol=1,ngntujumax )
+         END DO
+         CLOSE(666)
+         OPEN(UNIT = 777, FILE = TRIM(imfile))
+         DO irow = 1, ngntujumax
+            WRITE(777,fmt3)( DIMAG(gntuju(irow,icol,ic,ig)),icol=1,ngntujumax )
+         END DO
+         CLOSE(777)
+         !-- TODO: There should be a better way than this
+         WRITE(cmd, '("zip -9 -m -q gntuju.zip ", 2(1X,A))') &
+                     TRIM(refile), TRIM(imfile)
+         CALL EXECUTE_COMMAND_LINE(TRIM(cmd))
+         !--
+      END DO ! ic
+   END DO ! ig
+
+#endif /* HDF5 */
+
+END IF ! wproc
+#endif /* DUMPgntuju */
 
 deallocate(jl)
 deallocate(uju)
 deallocate(zm)
 return
 end
+
+!-------------------------------------------------------------------------------
+#ifdef _HDF5_
+
+SUBROUTINE writegntujuheader (fname)
+
+  USE modmain
+  USE mod_addons_q
+  USE mod_expigqr
+  USE mod_util
+  USE mod_hdf5
+
+  IMPLICIT NONE
+
+  ! Input argument
+  CHARACTER(*) :: fname
+
+  ! Internal variables
+  CHARACTER(LEN=100) :: pathp, pathq, pathqc
+  CHARACTER(LEN=2) :: c2
+  CHARACTER(LEN=3) :: c3
+  CHARACTER(LEN=4) :: c4
+  INTEGER :: iq, ig, ic, lmaxexp, nuju
+
+  CALL hdf5_create_file( fname )
+
+  CALL hdf5_create_group( fname, "/", "parameters" )
+  pathp = "/parameters"
+  CALL hdf5_write( fname, pathp, "ngridk", ngridk(1), (/3/) )
+  CALL hdf5_write( fname, pathp, "lmaxapw", lmaxapw )
+  lmaxexp = lmaxvr
+  CALL hdf5_write( fname, pathp, "lmaxexp", lmaxexp )
+  CALL hdf5_write( fname, pathp, "natmcls", natmcls )
+  CALL hdf5_write( fname, pathp, "nspecies", nspecies )
+  CALL hdf5_write( fname, pathp, "nvq", nvq )
+  CALL hdf5_write( fname, pathp, "gqsh", gqsh )
+  CALL hdf5_write( fname, pathp, "gqmax", gqmax )
+  CALL hdf5_write( fname, pathp, "nufr", nufr(0,1), (/ lmaxapw+1, nspecies /) )
+
+#ifdef _DUMPgntyyy_
+  CALL hdf5_write( fname, pathp, "lmmaxvr", lmmaxvr )
+#endif /* DUMPgntyyy */
+
+#ifdef _DUMPgntuju_
+  CALL hdf5_write( fname, pathp, "nufrmax", nufrmax )
+  CALL hdf5_write( fname, pathp, "lmmaxapw", lmmaxapw )
+  CALL hdf5_write( fname, pathp, "ngntujumax", ngntujumax )
+#endif /* DUMPgntuju */
+
+  CALL hdf5_create_group( fname, "/", "qpoints" )
+  CALL hdf5_write( fname, "/qpoints", "vqm", vqm(1,1), (/ 3, nvq /) )
+
+  DO iq = 1, nvq
+
+     WRITE( c3, '(I3.3)' ) iq
+     CALL hdf5_create_group( fname, "/qpoints", c3 )
+     pathq = "/qpoints/" // c3
+
+     CALL hdf5_write( fname, pathq, "ngq", ngq(iq) )
+     CALL hdf5_write( fname, pathq, "ngqsh", ngqsh(iq) )
+     CALL hdf5_write( fname, pathq, "gqshidx", gqshidx(1,iq), (/ ngq(iq) /) )
+     nuju=ngqsh(iq)*natmcls
+     CALL hdf5_write( fname, pathq, "nuju", nuju )
+
+#ifdef _DUMPgntuju_
+
+     CALL hdf5_create_group( fname, pathq, "class" )
+     DO ic = 1, natmcls
+        WRITE( c2, '(I2.2)' ) ic
+        CALL hdf5_create_group( fname, TRIM(pathq)//"/class", c2 )
+        pathqc = TRIM(pathq) // "/class/" // c2
+        CALL hdf5_create_group( fname, TRIM(pathqc), "gvectors" )
+        DO ig = 1, ngq(iq)
+           WRITE( c4, '(I4.4)' ) ig
+           CALL hdf5_create_group( fname, TRIM(pathqc)//"/gvectors", c4 )
+        END DO ! ig
+     END DO ! ic
+
+#endif /* DUMPgntuju */
+
+  END DO ! iq
+
+  RETURN
+
+END SUBROUTINE writegntujuheader
+
+#endif /* HDF5 */
