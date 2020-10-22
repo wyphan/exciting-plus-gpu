6e61370f305a6c99ec57a3faf00e5291e4b127e4
Wileam Phan <wphan@vols.utk.edu>
2020-10-21 23:41:07 -0400

    Update elk-wrapper.sh with Nsys 20.4 path on Summit

--- utilities/elk-wrapper.sh
+++ utilities/elk-wrapper.sh
@@ -4,13 +4,16 @@
 #export exe="elk-pgi-dbg-acc"
 export exe="elk-pgi-prof-acc"
 
+export pfx="crpa-NiO-PM"
+
 # Load NSight Systems 2020.4
-export NSYSDIR=/ccsopen/proj/gen148/eecm/nsight-systems-cli/2020.4.1/target-linux-ppc64le
+#export NSYSDIR=/ccsopen/proj/gen148/eecm/nsight-systems-cli/2020.4.1/target-linux-ppc64le
+export NSYSDIR=/ccs/proj/mat201/nsight-systems-cli/2020.4.1/target-linux-ppc64le
 export nsys=${NSYSDIR}/nsys
 
 # Only profile rank 0
 if [ "x${OMPI_COMM_WORLD_RANK}" == "x0" ]; then
-  ${nsys} profile --stats=true -t openacc -o "resp-RuCl3_%q{OMPI_COMM_WORLD_RANK}" ./${exe}
+  ${nsys} profile --stats=true -t openacc -o "${pfx}_%q{OMPI_COMM_WORLD_RANK}" ./${exe}
 else
   ./${exe}
 fi
