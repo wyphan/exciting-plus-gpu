4187a777d7fa0fa6395257f18aeeb78b030a5715
Wileam Phan <wphan@vols.utk.edu>
2020-10-26 17:15:42 -0400

    Update mod_nvtx to known working version

--- src/addons/mod_nvtx.f90
+++ src/addons/mod_nvtx.f90
@@ -1,17 +1,20 @@
 #ifdef _USE_NVTX_
 
 ! Fortran bindings for a small subset of the NVIDIA Tools Extensions library
-module nvtx
-  use iso_c_binding
+MODULE nvtx
+  USE iso_c_binding
+  IMPLICIT NONE
+
   public :: nvtxrangepusha, nvtxrangepop
   public :: nvtxrangepushaargb
+
   interface
 
     ! Annotate the timeline with a message
     ! Parameters:
     ! * string : the message in a string format
-    subroutine nvtxrangepusha(string) bind(C, name="nvtxRangePushA")
-      use iso_c_binding, only : c_char
+    subroutine nvtxRangePushA(string) bind(C, name="nvtxRangePushA")
+      use iso_c_binding, only: c_char
       character(kind=c_char) :: string(*)
     end subroutine nvtxrangepusha
 
@@ -19,7 +22,7 @@ module nvtx
     ! Parameters:
     ! * string : the message in a string format
     ! * argb   : the color in argb format (example: Z'FF880000'
-    subroutine nvtxrangepushaargb(string,argb) bind(C, name="_nvtxRangePushAARGB")
+    subroutine nvtxRangePushAArgb(string,argb) bind(C, name="nvtxRangePushAARGB")
       use iso_c_binding, only : c_char, c_int
       character(kind=c_char) :: string(*)
       integer(kind=c_int), value  :: argb
@@ -28,14 +31,12 @@ module nvtx
     ! Place a mark on the timeline with a message
     ! Parameters:
     ! * string : the message in a string format
-    ! NOT YET EXPOSED
     subroutine nvtxMarkA(string) bind(C, name="nvtxMarkA")
       use iso_c_binding, only : c_char
       character(kind=c_char) :: string(*)
     end subroutine
 
     ! Name an OS thread
-    ! NOT YET EXPOSED
     subroutine nvtxNameOsThread(tid, string) bind(C, name="nvtxNameOsThread")
       use iso_c_binding, only : c_int, c_char
       integer(kind=c_int) :: tid
@@ -52,40 +53,10 @@ MODULE mod_nvtx
   USE nvtx
   IMPLICIT NONE
 
-  PUBLIC :: nvtxStartRange, nvtxEndRange, nvtxRangePush, nvtxRangePop
-
-  INTERFACE
-     SUBROUTINE nvtxStartRange( label, color ) &
-          BIND(C, name="nvtxStartRange")
-       USE ISO_C_BINDING, ONLY: C_CHAR, C_INT
-       IMPLICIT NONE
-       CHARACTER(KIND=C_CHAR), INTENT(IN), VALUE :: label(*)
-       INTEGER(KIND=C_INT), INTENT(IN), VALUE :: color
-     END SUBROUTINE nvtxStartRange
-  END INTERFACE
-
-  INTERFACE
-     SUBROUTINE nvtxEndRange( label ) &
-          BIND(C, name="nvtxEndRange")
-       USE ISO_C_BINDING, ONLY: C_CHAR, C_INT
-       IMPLICIT NONE
-       CHARACTER(KIND=C_CHAR), INTENT(IN), VALUE :: label(*)
-     END SUBROUTINE nvtxEndRange
-  END INTERFACE
-
-  INTERFACE
-     SUBROUTINE nvtxRangePush() &
-          BIND(C, name="nvtxRangePush")
-       IMPLICIT NONE
-     END SUBROUTINE nvtxRangePush
-  END INTERFACE
-
-!  INTERFACE
-!     SUBROUTINE nvtxRangePop() &
-!          BIND(C, name="nvtxRangePop")
-!       IMPLICIT NONE
-!     END SUBROUTINE nvtxRangePop
-!  END INTERFACE
+  PUBLIC :: nvtxStartRange, nvtxEndRange
+  PUBLIC :: nvtxRangePushA, nvtxRangePushEx, nvtxRangePop
+  INTEGER, PRIVATE :: col(7) = [ Z'0000ff00', Z'000000ff', Z'00ffff00', Z'00ff00ff', Z'0000ffff', Z'00ff0000', Z'00ffffff']
+  CHARACTER(LEN=256), PRIVATE :: tempName
 
   TYPE, BIND(C) :: nvtxEventAttributes
      integer(C_INT16_T) :: version=1
@@ -99,6 +70,56 @@ MODULE mod_nvtx
      integer(C_INT) :: messageType=1 ! NVTX_MESSAGE_TYPE_ASCII = 1 
      type(C_PTR) :: message  ! ascii char
   END TYPE nvtxEventAttributes
+
+  INTERFACE
+     ! Push range with custom label and standard color
+     SUBROUTINE nvtxRangePushA( label, color ) &
+          BIND(C, name="nvtxRangePushA")
+       USE ISO_C_BINDING, ONLY: C_CHAR, C_INT
+       IMPLICIT NONE
+       CHARACTER(KIND=C_CHAR), INTENT(IN), VALUE :: label(*)
+       INTEGER(KIND=C_INT), INTENT(IN), VALUE :: color
+     END SUBROUTINE nvtxStartRange
+
+     ! Push range with custom label and custom color
+     SUBROUTINE nvtxRangePushEx( event ) &
+          BIND(C, name="nvtxRangePushEx")
+       USE ISO_C_BINDING, ONLY: C_CHAR, C_INT
+       IMPORT :: nvtxEventAttributes
+       IMPLICIT NONE
+       TYPE(nvtxEventAttributes) :: event
+     END SUBROUTINE nvtxEndRange
+  END INTERFACE
+
+  ! Pop range
+  INTERFACE
+     SUBROUTINE nvtxRangePop() &
+          BIND(C, name="nvtxRangePop")
+       IMPLICIT NONE
+     END SUBROUTINE nvtxRangePop
+  END INTERFACE
+
+CONTAINS
+
+  subroutine nvtxStartRange(name,id)
+    character(kind=c_char,len=*) :: name
+    integer, optional:: id
+    type(nvtxEventAttributes):: event
+
+    tempName=trim(name)//c_null_char
+
+    if ( .not. present(id)) then
+       call nvtxRangePush(tempName)
+    else
+       event%color=col(mod(id,7)+1)
+       event%message=c_loc(tempName)
+       call nvtxRangePushEx(event)
+    end if
+  end subroutine nvtxStartRange
+
+  subroutine nvtxEndRange
+    call nvtxRangePop
+  end subroutine nvtxEndRange
 
 END MODULE mod_nvtx
 
