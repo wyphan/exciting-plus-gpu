9649627b407b52ae66fa2e2734f186a8b9c27158
Wileam Phan <wphan@vols.utk.edu>
2020-10-26 13:59:39 -0400

    Add NVTX markers

---
+++ src/addons/mod_nvtx.f90
@@ -0,0 +1,32 @@
+#ifdef _USE_NVTX_
+
+MODULE mod_nvtx
+
+#include "nvToolsExt.h"
+
+  INTERFACE
+     SUBROUTINE nvtxStartRange(label,color)
+       BIND(C, name="nvtxStartRange")
+       IMPLICIT NONE
+       CHARACTER, INTENT(IN) :: label
+       INTEGER, INTENT(IN) :: color
+     END SUBROUTINE nvtxStartRange
+
+     SUBROUTINE nvtxEndRange
+       BIND(C, name="nvtxEndRange")
+       IMPLICIT NONE
+     END SUBROUTINE nvtxEndRange
+
+     SUBROUTINE nvtxRangePush
+       BIND(C, name="nvtxRangePush")
+       IMPLICIT NONE
+     END SUBROUTINE nvtxRangePush
+
+     SUBROUTINE nvtxRangePop
+       BIND(C, name="nvtxRangePop")
+       IMPLICIT NONE
+     END SUBROUTINE nvtxRangePop
+
+  END INTERFACE
+
+#endif /* _USE_NVTX_ */
