7599c1e6c9cec0237272378ad960c5b0d02194c2
Wileam Phan <wphan@vols.utk.edu>
2021-01-04 13:33:58 -0500

    Fix mod_expigqr::genmegq() for when GPUDirect is not enabled

--- src/addons/expigqr/mod_expigqr.f90
+++ src/addons/expigqr/mod_expigqr.f90
@@ -318,9 +318,7 @@ megqblh(:,:,:)=zzero
 
 allocate(wfsvmt_jk(lmmaxapw,nufrmax,natmtot,nspinor,nstsv))
 
-#ifdef _GPUDIRECT_
 !$ACC DATA CREATE( wfsvmt_jk )
-#endif /* _GPUDIRECT_ */
 
 allocate(wfsvit_jk(ngkmax,nspinor,nstsv))
 allocate(igkignr_jk(ngkmax))
@@ -466,10 +464,8 @@ if (wproc) then
   call flushifc(150)
 endif
 
-#ifdef _GPUDIRECT_
 ! wfsvmt_jk
 !$ACC END DATA
-#endif /* _GPUDIRECT_ */
 
 deallocate(wfsvmt_jk)
 deallocate(wfsvit_jk)
