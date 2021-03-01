3ef1d0040b774d386bc0e0d0db18a3dcfdf2e044
Wileam Phan <wphan@vols.utk.edu>
2021-03-01 15:36:02 -0500

    Update Summit compile script

--- compile-summit.sh
+++ compile-summit.sh
@@ -367,7 +367,7 @@ if [ "x${BUILDELK}" == "x1" ]; then
 
   # Load CUDA
   if [ "x${USECUDA}" == "x1" ]; then
-    module load cuda/11.1.1
+    module load cuda/${CUDAVER}
     echo "Using CUDA (for nvTX)"
   fi
 
